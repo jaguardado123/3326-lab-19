516f498d2bd309009c3b0dcd710b158abcd5f56e
Joselito A Guardado <36009999+jaguardado123@users.noreply.github.com>
2024-07-30 17:03:41 +0000

    Updated tests

--- src/CarTest.java
+++ src/CarTest.java
@@ -7,37 +7,22 @@ public class CarTest {
 	@Test
 	public void testing_attr_make() {
 		Car obj = new Car();
-		try {
-			obj.make = "Ford";
-			assertEquals(true, false);
-		} catch(Exception e) {
-			obj.set_make("Ford");
-			assertEquals("Ford", obj.get_make());
-		}
+		obj.set_make("Ford");
+		assertEquals("Ford", obj.get_make());
 	}
 
 	@Test
 	public void testing_attr_model() {
 		Car obj = new Car();
-		try {
-			obj.model = "Mustang";
-			assertEquals(true, false);
-		} catch(Exception e) {
-			obj.set_model("Mustang");
-			assertEquals("Mustang", obj.get_model());
-		}
+		obj.set_model("Mustang");
+		assertEquals("Mustang", obj.get_model());
 	}
 
 	@Test
 	public void testing_attr_year() {
 		Car obj = new Car();
-		try {
-			obj.year = 2000;
-			assertEquals(true, false);
-		} catch(Exception e) {
-			obj.set_year(2000);
-			assertEquals(2000, obj.get_year());
-		}
+		obj.set_year(2000);
+		assertEquals(2000, obj.get_year());
 	}
 
 	@Test
